4908041c8d570dc9af461027f4f7c7e63d5a7501
Javi H. Gil <javihgil@softspring.es>
2024-04-25 11:11:38 +0200

    Add bundle type hint

--- src/SfsMailerBundle.php
+++ src/SfsMailerBundle.php
@@ -15,7 +15,7 @@ class SfsMailerBundle extends Bundle
         return \dirname(__DIR__);
     }
 
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
